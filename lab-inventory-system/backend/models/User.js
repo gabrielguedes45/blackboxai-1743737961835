f2847725fa5f023e9df4e8818bd32dde7ac69d08
gabrielguedes45 <gabrielggguedes@gmail.com>
2025-04-04 00:39:42 -0300

    Add lab-inventory-system/backend/models/User.js 1743737982009

---
+++ lab-inventory-system/backend/models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  return sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('client', 'secretary', 'admin'),
+      defaultValue: 'client',
+      allowNull: false
+    }
+  }, {
+    tableName: 'users',
+    timestamps: true
+  });
+};
